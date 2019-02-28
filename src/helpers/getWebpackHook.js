280f251413b50d89a311f7a46c22edd1ffe22059
dangreen <danon0404@gmail.com>
2019-02-28 18:24:14 +0700

    add src

---
+++ src/helpers/getWebpackHook.js
@@ -0,0 +1,32 @@
+import path from 'path';
+import babelRegister from '@babel/register';
+
+const scriptsPath = path.join(process.cwd(), 'scripts');
+const hookPath = path.join(scriptsPath, 'webpack.js');
+const emptyHook = {
+	base:  _ => _,
+	dev:   _ => _,
+	build: _ => _
+};
+
+babelRegister({
+	babelrc: false,
+	only:    [
+		file => !file.indexOf(scriptsPath)
+	],
+	presets: [
+		['babel-preset-trigen', {
+			targets:  { node: 'current' },
+			commonjs: true
+		}]
+	]
+});
+
+export default function getWebpackHook() {
+
+	try {
+		return require(hookPath); // eslint-disable-line
+	} catch (err) {
+		return emptyHook;
+	}
+}
