6032583d26c2538cb3ecf4ff19c5f62b4ec83655
dangreen <danon0404@gmail.com>
2019-03-01 00:47:28 +0700

    fix storybook webpack config

--- src/storybook/webpack.config.js
+++ src/storybook/webpack.config.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import fs from 'fs';
-import webpackConfig from '../configs/webpack';
+import * as webpackConfig from '../configs/webpack';
 
 module.exports = configureStorybook;
 
