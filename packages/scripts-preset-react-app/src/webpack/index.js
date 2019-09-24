2f199322c68da82257bad3a0e39b99cf16d62f9f
dangreen <danon0404@gmail.com>
2019-09-24 21:12:43 +0700

    oops

--- packages/scripts-preset-react-app/src/webpack/index.js
+++ packages/scripts-preset-react-app/src/webpack/index.js
@@ -244,7 +244,7 @@ export function build(params) {
 				excludeAssets
 			}),
 			new ScriptHtmlPlugin({
-				defaultAttribute: 'async'
+				defaultAttribute: 'defer'
 			}),
 			new ExcludeHtmlPlugin()
 		] }
