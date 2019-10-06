d7ac815f3736e558d3fe534140a74e3815dd344b
dangreen <danon0404@gmail.com>
2019-10-06 19:55:42 +0700

    DISABLE_BROWSER_SYNC mod

--- packages/scripts-preset-react-app/src/configs/browserSync.js
+++ packages/scripts-preset-react-app/src/configs/browserSync.js
@@ -1,11 +1,12 @@
 
 export default {
-	server:     'build',
-	ui:         process.env.DISABLE_BROWSER_SYNC
-		? false
-		: { port: 3001 },
-	ghostMode:  !process.env.DISABLE_BROWSER_SYNC,
-	open:       false,
-	notify:     false,
-	logSnippet: false
+	server:         'build',
+	ui:             !process.env.DISABLE_BROWSER_SYNC && { port: 3001 },
+	ghostMode:      !process.env.DISABLE_BROWSER_SYNC,
+	open:           false,
+	notify:         false,
+	logSnippet:     false,
+	snippetOptions: {
+		blacklist: process.env.DISABLE_BROWSER_SYNC && ['/', /.*/]
+	}
 };
